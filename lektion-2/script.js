1eb44144245f19dcea3d8d455ec615b62c655911
teethfrog <kasianoa@arcada.fi>
2022-11-07 15:46:00 +0200

    Lektion 2 avklarad

--- lektion-2/script.js
+++ lektion-2/script.js
@@ -1,1 +1,31 @@
-console.log("Hello from js-file!")
+console.log("Hello from js-file!");
+
+function minFunktion() {
+    console.log("Funktionen kördes!");
+}
+
+minFunktion();
+
+function buttonHandler() {
+    console.log("Du klickade här!");
+    alert("Du klickade på knappen.")
+}
+
+function secondButton() {
+    const name = prompt("Whats your name?");
+
+    alert("Hello "+ name);
+}
+
+function thirdButton() {
+    let name = document.querySelector("#name").value;
+    //const name = document.getElementById("name").value; -- Samma sak! Men specifikt för ID.
+    if (name == "Alexander") {
+        name = name + " Kasianov";
+    } else if (name == "kasianoa") {
+        name = "Alexander Kasianov"
+    } else {
+        console.log("Okänt efternamn!");
+    }
+    document.querySelector("#hello-output").innerText = `Hello ${name}!`;
+}
